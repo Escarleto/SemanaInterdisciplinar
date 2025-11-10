c6d3a97433734be7a55dd4bfb8067c71035bb06a
SergioTomaz <lucascliberato31@gmail.com>
2025-11-10 15:11:10 +0000

    The Return of the floor

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -1,16 +1,26 @@
+using UnityEditor;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject Tile;
+    private BoxCollider StageArea;
+
     void Start()
     {
-        
-    }
+        StageArea = GetComponent<BoxCollider>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < StageArea.size.x; i++)
+        {
+            for (int j = 0; j < StageArea.size.z; j++)
+            {
+                Vector3 position = new Vector3(
+                    transform.position.x - StageArea.size.x / 2 + i + 0.5f,
+                    0f,
+                    transform.position.z - StageArea.size.z / 2 + j + 0.5f
+                );
+                Instantiate(Tile, position, Quaternion.identity, transform);
+            }
+        }
     }
 }
